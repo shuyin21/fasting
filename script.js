f3ed3588555eb267a1a59788ce69265911471154
shuyin21 <shuyindev@gmail.com>
2022-05-23 17:00:15 +0100

    homepage mobile version created

---
+++ script.js
@@ -0,0 +1,9 @@
+const footerP = document.querySelector('#footer p');
+
+
+let date = () => {
+    let d = new Date();
+    let year = d.getFullYear();
+    footerP.innerHTML = `Life Style Change App ${year}`
+}
+date();
