af912adf289c07933eb9a6af324e698a51102d53
shuyin21 <shuyindev@gmail.com>
2022-05-24 17:25:30 +0100

    food list created

---
+++ Food/food.js
@@ -0,0 +1,47 @@
+const footerP = document.querySelector('#footer p');
+function dateFunc() {
+    let d = new Date();
+    let year = d.getFullYear();
+    footerP.innerHTML = `Life Style Change App ${year}`;
+}
+dateFunc();
+
+
+const addBtn = document.getElementById('add-btn');
+const listBox = document.querySelector('#list-box ul');
+let foodInput = document.getElementById('foodInput');
+let foodQt = document.getElementById('foodQt');
+let size = document.getElementById('size');
+
+
+
+addBtn.addEventListener('click', () => {
+
+    crtListItem(foodInput.value, foodQt.value, size.value);
+    foodInput.value = '';
+    foodQt.value = '';
+    size.value = '';
+
+});
+// Creating the list Item
+const crtListItem = (foodInput, foodQt, size) => {
+    if (foodInput.length > 12) {
+        foodInput = `${foodInput.substring(0, 11).toUpperCase()}..`;
+    }
+    let li = document.createElement('li');
+    let h4 = document.createElement('h4');
+    let p = document.createElement('p');
+    let btn = document.createElement('button');
+    listBox.appendChild(li);
+    li.appendChild(h4);
+    li.appendChild(p);
+    li.appendChild(btn);
+    h4.innerText = foodInput;
+    p.innerHTML = `${foodQt} ${size}`;
+    btn.innerText = 'X';
+    btn.addEventListener('click', () => {
+        li.remove();
+    });
+}
+
+
--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 const footerP = document.querySelector('#footer p');
+const workoutBtn = document.getElementById('workout');
+const foodBtn = document.getElementById('food');
+const fastingBtn = document.getElementById('fasting');
 
+const buttonsHolder = [workoutBtn, foodBtn, fastingBtn];
 
 let date = () => {
     let d = new Date();
@@ -7,3 +11,18 @@ let date = () => {
     footerP.innerHTML = `Life Style Change App ${year}`
 }
 date();
+
+buttonsHolder.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        if (btn.id === 'workout') {
+            document.location.href = '/Workout/workout.html';
+        }
+        else if (btn.id === 'fasting') {
+            document.location.href = '/Fasting/fasting.html';
+        }
+        else {
+            document.location.href = '/Food/food.html';
+        }
+    })
+})
+
