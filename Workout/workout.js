63a842c1026c236e5fb733c474e3acdfbcccc029
shuyin21 <shuyindev@gmail.com>
2022-06-02 18:21:19 +0100

    workout base is done

--- Workout/workout.js
+++ Workout/workout.js
@@ -1,3 +1,39 @@
-import { loggedIn } from "../firebase";
+const weekDiv = document.getElementById('week-select');
+const weekSelector = document.getElementById('week-selector');
+const mainDiv = document.getElementById('selection');
+const week1Btn = document.querySelector('#week-1');
+const week2Btn = document.querySelector('#week-2');
+const week3Btn = document.querySelector('#week-3');
+const week4Btn = document.querySelector('#week-4');
 
-console.log(loggedIn);
+function weekselectFunc(x) {
+    weekDiv.classList.add('non-display');
+    console.log(x);
+    let wButtons = document.querySelector('.w-buttons');
+    wButtons.classList.remove('non-display');
+    btnShower(Number(x));
+
+}
+
+function btnShower(x) {
+    switch (x) {
+        case 1:
+            week1Btn.classList.remove('non-display');
+            break;
+        case 2: week1Btn.classList.remove('non-display');
+            week2Btn.classList.remove('non-display');
+            break;
+        case 3:
+            week1Btn.classList.remove('non-display');
+            week2Btn.classList.remove('non-display');
+            week3Btn.classList.remove('non-display');
+            break;
+        case 4:
+            week1Btn.classList.remove('non-display');
+            week2Btn.classList.remove('non-display');
+            week3Btn.classList.remove('non-display');
+            week4Btn.classList.remove('non-display');
+        default:
+            break;
+    }
+}
